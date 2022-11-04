59a5c0ed35e9760478fdcdb666451b2697b5d5f0
Harun <harun_artin@hotmail.com>
2022-11-05 01:01:09 +0300

    .

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+// import { Link } from 'react-router-dom';
+
+export function HomePage() {
+    return (
+        <div className="App">
+            <div className='container'>
+                <Navbar></Navbar>
+                asd
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+}
