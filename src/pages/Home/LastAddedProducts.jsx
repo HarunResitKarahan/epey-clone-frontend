67f75ad8d77995ecbcf1879acdcff5f20a99c7d5
Harun <harun_artin@hotmail.com>
2022-11-04 17:44:43 +0300

    .

--- src/pages/Home/LastAddedProducts.jsx
+++ src/pages/Home/LastAddedProducts.jsx
@@ -5,16 +5,21 @@ import productImage from '../../assets/images/b_philips-xc8049-01-1.webp'
 function LastAddedProducts() {
     return (
         <div className='lastAddedProducts p-2 py-1 mt-4 border'>
-            <h5>Son Eklenen Ürünler</h5>
+            <h5 className='py-2'>Son Eklenen Ürünler</h5>
             <div className='row'>
                 {[...Array(6)].map((e, i) => (
-                    <div key={i} className='lastAddedProductsItems col-sm-4 mb-2' >
-                        <div className='lastAddedProductsItem p-2'>
+                    <div key={i} className='lastAddedProductsItems col-sm-4 mb-2 rounded' >
+                        <div className='lastAddedProductsItem p-2 py-4'>
                             <div className='productImage'>
                                 <img src={productImage} alt="" />
                             </div>
-                            <div className='productSpecs'>
-                                <p className='header'>Philips XC8049/01 Şarjlı Süpürge</p>
+                            <div className='productSpecs px-2'>
+                                <p className='header mb-1'>Philips XC8049/01 Şarjlı Süpürge</p>
+                                <div className='h-100 d-flex flex-column justify-content-between'>
+                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Ürün Markası: <span style={{ fontWeight: "400" }}>Philips</span></p>
+                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Üretim Yılı: <span style={{ fontWeight: "400" }}>2022</span></p>
+                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Garanti Süresi: <span style={{ fontWeight: "400" }}>4 Yıl</span></p>
+                                </div>
                             </div>
                         </div>
                     </div>
