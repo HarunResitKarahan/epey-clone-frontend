7136805e2a8681b66c739244451f727996cf9d53
Harun <harun_artin@hotmail.com>
2022-11-04 17:29:54 +0300

    .

--- src/pages/Home/LastAddedProducts.jsx
+++ src/pages/Home/LastAddedProducts.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './LastAddedProducts.css'
+import productImage from '../../assets/images/b_philips-xc8049-01-1.webp'
 
 function LastAddedProducts() {
     return (
-        <div>LastAddedProducts</div>
+        <div className='lastAddedProducts p-2 py-1 mt-4 border'>
+            <h5>Son Eklenen Ürünler</h5>
+            <div className='row'>
+                {[...Array(6)].map((e, i) => (
+                    <div key={i} className='lastAddedProductsItems col-sm-4 mb-2' >
+                        <div className='lastAddedProductsItem p-2'>
+                            <div className='productImage'>
+                                <img src={productImage} alt="" />
+                            </div>
+                            <div className='productSpecs'>
+                                <p>Philips XC8049/01 Şarjlı Süpürge</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
