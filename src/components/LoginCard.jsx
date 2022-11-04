bd4e87dd3b2564c3e58c4f0755171b677dbcb563
Harun <harun_artin@hotmail.com>
2022-11-05 01:37:54 +0300

    .

---
+++ src/components/LoginCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LoginCard() {
+    return (
+        <div className='d-flex flex-column justify-content-center align-items-center mt-4'>
+            <div className='d-flex'>
+                <div className='LoginButton py-2 px-5 me-1 rounded'>
+                    Giriş
+                </div>
+                <div className='signInButton py-2 px-5 ms-1 rounded'>
+                    Üye Ol
+                </div>
+            </div>
+            <div>
+                asd
+            </div>
+        </div>
+    )
+}
+
+export default LoginCard
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,7 @@
 import './style.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import LoginCard from '../../components/LoginCard';
 // import { Link } from 'react-router-dom';
 
 export function Login() {
@@ -8,7 +9,7 @@ export function Login() {
         <div className="App">
             <div className='container'>
                 <Navbar></Navbar>
-                asd
+                <LoginCard></LoginCard>
             </div>
             <Footer></Footer>
         </div>
