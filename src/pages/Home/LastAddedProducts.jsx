49690564f63e3387dbdb38ff85d236d5c679bb3e
Harun <harun_artin@hotmail.com>
2022-11-04 17:47:44 +0300

    .

--- src/pages/Home/LastAddedProducts.jsx
+++ src/pages/Home/LastAddedProducts.jsx
@@ -10,15 +10,17 @@ function LastAddedProducts() {
                 {[...Array(6)].map((e, i) => (
                     <div key={i} className='lastAddedProductsItems col-sm-4 mb-2 rounded' >
                         <div className='lastAddedProductsItem p-2 py-4'>
-                            <div className='productImage'>
-                                <img src={productImage} alt="" />
-                            </div>
-                            <div className='productSpecs px-2'>
-                                <p className='header mb-1'>Philips XC8049/01 Şarjlı Süpürge</p>
-                                <div className='h-100 d-flex flex-column justify-content-between'>
-                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Ürün Markası: <span style={{ fontWeight: "400" }}>Philips</span></p>
-                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Üretim Yılı: <span style={{ fontWeight: "400" }}>2022</span></p>
-                                    <p style={{ textAlign: "left", fontSize: "12px" }}>Garanti Süresi: <span style={{ fontWeight: "400" }}>4 Yıl</span></p>
+                            <div className='row'>
+                                <div className='col-md-5 productImage'>
+                                    <img src={productImage} alt="" />
+                                </div>
+                                <div className='col-md-7 productSpecs px-2'>
+                                    <p className='header mb-1'>Philips XC8049/01 Şarjlı Süpürge</p>
+                                    <div className='h-100 d-flex flex-column justify-content-between'>
+                                        <p style={{ textAlign: "left", fontSize: "12px" }}>Ürün Markası: <span style={{ fontWeight: "400" }}>Philips</span></p>
+                                        <p style={{ textAlign: "left", fontSize: "12px" }}>Üretim Yılı: <span style={{ fontWeight: "400" }}>2022</span></p>
+                                        <p style={{ textAlign: "left", fontSize: "12px" }}>Garanti Süresi: <span style={{ fontWeight: "400" }}>4 Yıl</span></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
