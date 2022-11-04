4225c76d2b4be6eb4b3eaed6f7c018140037fb2d
Harun <harun_artin@hotmail.com>
2022-11-04 17:52:14 +0300

    .

--- src/pages/Home/FeaturedNews.jsx
+++ src/pages/Home/FeaturedNews.jsx
@@ -3,14 +3,14 @@ import './FeaturedNews.css'
 
 function FeaturedNews() {
     return (
-        <div className='featuredNews col-sm-4 ps-0'>
+        <div className='featuredNews col-sm-4 ps-0 mt-2 mt-sm-0'>
             <div className='featuredNewsList'>
                 {[...Array(9)].map((e, i) => (
                     <div key={i} className='featuredNewsItems'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#d96140" className="bi bi-caret-right-fill me-1" viewBox="0 0 16 16">
                             <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
                         </svg>
-                        <div className='featuredNewsItemsContent w-100 p-2'>
+                        <div className='featuredNewsItemsContent w-100 p-2 mb-2 mb-sm-0'>
                             En Ucuz Sıcak Hava Fritözleri (Air Fryer)
                         </div>
                     </div>
