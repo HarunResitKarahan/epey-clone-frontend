7bc0c1e25942e3f8b8ed91d9bd370b0336f9ea1b
Harun <harun_artin@hotmail.com>
2022-11-05 01:42:08 +0300

    .

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LoginCard() {
     return (
-        <div className='d-flex flex-column justify-content-center align-items-center mt-4'>
+        <div className='d-flex flex-column justify-content-center align-items-center mt-4 p-2 m-auto' style={{ width: "fit-content" }}>
             <div className='d-flex'>
                 <div className='LoginButton py-2 px-5 me-1 rounded'>
                     Giriş
@@ -11,8 +11,13 @@ function LoginCard() {
                     Üye Ol
                 </div>
             </div>
-            <div>
-                asd
+            <div className='Login mt-3'>
+                <div className='eMail'>
+                    <input id="" type="email" />
+                </div>
+                <div className='password'>
+                    <input id="" type="password" />
+                </div>
             </div>
         </div>
     )
