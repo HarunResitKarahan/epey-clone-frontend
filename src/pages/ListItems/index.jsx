fd05cb44efcb19e3a3b9c0b0b73ba475f22597bd
Harun <harun_artin@hotmail.com>
2022-11-05 10:04:03 +0300

    .

--- src/pages/ListItems/index.jsx
+++ src/pages/ListItems/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar'
 import './style.css'
 function ListItems() {
     return (
-        <div>ListItems</div>
+        <div className="App">
+            <div className='container'>
+                <Navbar></Navbar>
+                asd
+            </div>
+            <Footer></Footer>
+        </div>
     )
 }
 
