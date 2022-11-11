f0c93902076d833a2735aa5187b8b31845b60586
Harun <harun_artin@hotmail.com>
2022-11-11 20:57:31 +0300

    .

--- src/pages/Admin/Components/EditPopUp.jsx
+++ src/pages/Admin/Components/EditPopUp.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 function EditPopUp(props) {
     return (
         <div className='EditPopUp'>
-            <div className='EditPopUpCard p-3 rounded'>
+            <form onSubmit={(e) => e.preventDefault()} className='EditPopUpCard p-3 rounded'>
                 <h5 className='mb-3'>Ürünü Güncelle</h5>
-                <input type="text" className='form-control mb-2' placeholder='Ürün Adı' />
-                <input type="text" className='form-control mb-2' placeholder='Fiyatı' />
-                <input type="text" className='form-control mb-2' placeholder='Kategori' />
+                <input type="text" className='form-control mb-2' placeholder='Ürün Adı' required />
+                <input type="text" className='form-control mb-2' placeholder='Fiyatı' required />
+                <select className="form-select" aria-label="Default select example" required>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
                 <button type='submit' className='btn btn-primary w-100 mt-2'>Kaydet</button>
-            </div>
+            </form>
         </div>
     )
 }
