4bd3474a49a9b2cb255aae216f0d294044785e60
Harun <harun_artin@hotmail.com>
2022-11-11 20:55:59 +0300

    .

--- src/pages/Admin/Components/EditPopUp.jsx
+++ src/pages/Admin/Components/EditPopUp.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function EditPopUp(props) {
     return (
         <div className='EditPopUp'>
-            {props.selectedProduct}
+            <div className='EditPopUpCard p-3 rounded'>
+                <h5 className='mb-3'>Ürünü Güncelle</h5>
+                <input type="text" className='form-control mb-2' placeholder='Ürün Adı' />
+                <input type="text" className='form-control mb-2' placeholder='Fiyatı' />
+                <input type="text" className='form-control mb-2' placeholder='Kategori' />
+                <button type='submit' className='btn btn-primary w-100 mt-2'>Kaydet</button>
+            </div>
         </div>
     )
 }
