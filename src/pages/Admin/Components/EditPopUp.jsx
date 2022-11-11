06a055fb30094fd3f42ee9523ea993bcb2fdf8e4
Harun <harun_artin@hotmail.com>
2022-11-11 21:23:37 +0300

    .

--- src/pages/Admin/Components/EditPopUp.jsx
+++ src/pages/Admin/Components/EditPopUp.jsx
@@ -1,9 +1,28 @@
+import axios from 'axios';
 import React from 'react'
 
 function EditPopUp(props) {
+    const apiUrl = "http://localhost:8586/"
+    const editProduct = (e) => {
+        axios.put(apiUrl + 'api/Product/' + props.selectedProduct, {
+            productId: Number(props.selectedProduct),
+            productName: String(e.target[0].value),
+            productPrice: Number(e.target[1].value),
+            subCategoryId: Number(e.target[2].value)
+        })
+            .then(function (response) {
+                console.log(response)
+                if (response.status === 204) {
+                    props.setShowEditPopUp(false)
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     return (
         <div className='EditPopUp' onMouseDown={(e) => { if (e.target.className === 'EditPopUp') props.setShowEditPopUp(false) }}>
-            <form onSubmit={(e) => e.preventDefault()} className='EditPopUpCard p-3 rounded'>
+            <form onSubmit={(e) => { e.preventDefault(); editProduct(e) }} className='EditPopUpCard p-3 rounded'>
                 <h5 className='mb-3'>Ürünü Güncelle</h5>
                 <input type="text" className='form-control mb-2' placeholder='Ürün Adı' required />
                 <input type="text" className='form-control mb-2' placeholder='Fiyatı' required />
