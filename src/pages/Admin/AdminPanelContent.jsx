397a686ff7e847b6c9e5ea5a2dd1ecca5babf36c
Harun <harun_artin@hotmail.com>
2022-11-11 19:37:32 +0300

    .

--- src/pages/Admin/AdminPanelContent.jsx
+++ src/pages/Admin/AdminPanelContent.jsx
@@ -3,11 +3,51 @@ import AdminPanelNavbar from './Components/AdminPanelNavbar'
 // import Navbar from '../../components/Navbar'
 
 function AdminPanelContent() {
+    let parse = require('html-react-parser')
+    const generalCard = [
+        {
+            title: "Ürün Sayısı",
+            count: 105,
+            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+                    <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                </svg>`
+        },
+        {
+            title: "Üye Sayısı",
+            count: 30,
+            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-person-video" viewBox="0 0 16 16">
+                    <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                    <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1.202Z"/>
+                </svg>`
+        },
+        {
+            title: "Kategori Sayısı",
+            count: 5,
+            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
+                    <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
+                    <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
+                    </svg>`
+        },
+    ]
     return (
         <>
             <AdminPanelNavbar></AdminPanelNavbar>
             <div className='container'>
-                <h5 className='my-3'>Panel Genel Görünüm</h5>
+                <h3 className='my-4 text-secondary'>Panel Genel Görünüm</h3>
+                <div className='generalCard row'>
+                    {generalCard.map((e, i) => (
+                        <div key={i} className='generalCards col-md-4 mb-2 m-md-0'>
+                            <div className='content p-5 d-flex justify-content-center flex-column align-items-center'>
+                                <div className='mb-2'>
+                                    {parse(String(e.svg))}
+                                </div>
+                                <h4 align="center">{e.title}</h4>
+                                <h3>{e.count}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <h3 className='my-4 text-secondary'>Ürünler</h3>
             </div>
         </>
     )
