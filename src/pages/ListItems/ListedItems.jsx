c4094abf90e8b4b2802ec2d46f40b3839f633c3c
Harun <harun_artin@hotmail.com>
2022-11-13 14:35:59 +0300

    .

--- src/pages/ListItems/ListedItems.jsx
+++ src/pages/ListItems/ListedItems.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
 function ListedItems(props) {
+    const filteredProducts = props.products.filter((e) => Number(e.subCategoryId) === Number(props.categoryId))
     return (
         <div className='col-sm-8 col-lg-9'>
-            {props.products
-                .filter((e) => Number(e.subCategoryId) === Number(props.categoryId))
-                .map((elmnt, i) => (
+            {
+                filteredProducts.map((elmnt, i) => (
                     <p key={i}>{elmnt.productName}</p>
                 ))
             }
+            {filteredProducts.length === 0 ?
+                <div>Ürün yok</div>
+                : ''}
         </div>
     )
 }
