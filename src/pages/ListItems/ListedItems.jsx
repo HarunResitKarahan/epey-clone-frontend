a11083f969478b13592ee58a7c43605df14734aa
Harun <harun_artin@hotmail.com>
2022-11-13 14:25:31 +0300

    .

--- src/pages/ListItems/ListedItems.jsx
+++ src/pages/ListItems/ListedItems.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function ListedItems(props) {
     return (
         <div className='col-sm-8 col-lg-9'>
-            {props.categoryId}
             {props.products
                 .filter((e) => Number(e.subCategoryId) === Number(props.categoryId))
                 .map((elmnt, i) => (
